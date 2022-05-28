73e25a4ae5303575a03f26c9dd83b279f996d162
Kim Hongil <hoil20707@gmail.com>
2022-05-28 11:41:37 +0900

    paging update

--- src/main/java/com/spring/ex/service/PagingService.java
+++ src/main/java/com/spring/ex/service/PagingService.java
@@ -1,14 +1,11 @@
 package com.spring.ex.service;
 
-import java.util.HashMap;
-
 import javax.servlet.http.HttpServletRequest;
 
 import com.spring.ex.dto.PagingDTO;
 
 public class PagingService {
 	private PagingDTO paging;
-	private HashMap<String, Integer> map;
 	private int nowPage;
 	public PagingService(HttpServletRequest request, int totalCount, int pageSize) {
 		int page = request.getParameter("page") == null ? 1 : Integer.parseInt(request.getParameter("page"));
@@ -32,10 +29,5 @@ public class PagingService {
 	
 	public int getNowPage() {
 		return nowPage;
-	}
-	
-	// 게시물을 불러오기 위한 조건 map
-	public HashMap<String, Integer> getMap() {
-		return map;
 	}
 }
